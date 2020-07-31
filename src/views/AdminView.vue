fc5fab1ab5c3ba14a812f159ee707ae088edc802
NGY198888 <782413674@qq.com>
2020-07-31 12:08:02 +0800

    提交修改 右上角个人设置

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -13,7 +13,7 @@
           <el-dropdown @command="handleCommand">
             <i class="el-icon-setting" style="margin-right: 15px"></i>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item icon="el-icon-user-solid" command="settingUser">账号设置</el-dropdown-item>
+              <el-dropdown-item :icon="dd.icon" :command="dd.command" v-for=" dd in dropdownArr" :key="dd.command">{{dd.name}}</el-dropdown-item>
               <el-dropdown-item icon="el-icon-eleme" command="logout">退出系统</el-dropdown-item>
               <!-- <el-dropdown-item>删除</el-dropdown-item> -->
             </el-dropdown-menu>
@@ -31,7 +31,8 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState,mapMutations } from "vuex";
+import types from '@/store/types';
 // import TableView from '@/components/TableView';
 import Menu from './admin/Menu';
 import TabsView from './TabsView';
@@ -48,18 +49,29 @@ export default {
   },
   data () {
     return {
-     
+        dropdownArr:[
+          {
+            name:'账号设置',
+            command:'settingUser',
+            icon:'el-icon-user-solid',
+            route:'form/user.update'
+          }
+
+        ]
     }
   },
   methods:{
+      ...mapMutations([types.site_add_tab]),
       handleCommand(command) {
-           switch(command){
-             case "settingUser":
-               this.$router.push("/form/user.setting");
-               break;
-             case "logout":
-               this.$router.push("/logout");
-               break;
+           if(command=='logout'){
+              this.$router.push("/logout");
+           }else{
+              let dd= this.dropdownArr.find(item=>item.command==command)
+              this[types.site_add_tab]({tab:{
+                    name:dd.name,
+                    route:dd.route
+              }});
+              this.$router.push(dd.route);
            }
       }
   },
