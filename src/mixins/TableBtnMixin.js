4e6df430fc9153c98eaebc0a8427b1e81732d0e3
NGY198888 <782413674@qq.com>
2020-08-25 17:54:16 +0800

    提交修改 导出

--- src/mixins/TableBtnMixin.js
+++ src/mixins/TableBtnMixin.js
@@ -153,6 +153,39 @@ export default {
                   this.query()
               })
           }
+          else if(btn.action=='Export'){
+            let query={
+                page:this.gridConfig.page,
+                perPage:this.gridConfig.pageSize,
+                sort:this.gridConfig.sortStr,
+                where:this.gridConfig.tableSearchFields,
+            }
+            request.get(`/${this.resource}/exportAll`,{
+               params:{query:JSON.stringify(query)},
+               responseType: 'blob',
+              }).then(res=>{
+                  if (!res) {
+                      this.$message.error("下载失败");
+                      return false;
+                  }
+                  const blob = new Blob([res.data], {type: 'application/vnd.openxmlformats'});
+                  const downloadElement = document.createElement('a');
+                  const href = window.URL.createObjectURL(blob);
+                  let contentDisposition = res.headers['content-disposition'];  //从response的headers中获取filename, 后端response.setHeader("Content-disposition", "attachment; filename=xxxx.docx") 设置的文件名;
+                  console.log(contentDisposition);
+                  // let patt = new RegExp("filename=([^;]+\\.[^\\.;]+);*");
+                  // let result = patt.exec(contentDisposition);
+                  // let filename = decodeURI(result[1]);
+                  let filename='a.xlsx'
+                  downloadElement.style.display = 'none';
+                  downloadElement.href = href;
+                  downloadElement.download = filename ; //下载后文件名
+                  document.body.appendChild(downloadElement);
+                  downloadElement.click(); //点击下载
+                  document.body.removeChild(downloadElement); //下载完成移除元素
+                  window.URL.revokeObjectURL(href); //释放掉blob对象
+              })
+          }
         },
         deleteLocal(btn,value,row){
           let selection=this.selection
